58795f8b2b8257cafe3e7cc9ddbedce9c08e244c
ankitak <96825213+ankitaKhedekar@users.noreply.github.com>
2024-04-29 22:47:57 +0530

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'discover', label: 'Discover' },
+    { href: '/', key: 'specialDeals', label: 'Special Deals' },
+    { href: '/', key: 'contacts', label: 'Contact' },
+  ];
