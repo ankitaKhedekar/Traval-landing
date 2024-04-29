520433f294631d01482009bcec9626564a2ba1dd
ankitak <96825213+ankitaKhedekar@users.noreply.github.com>
2024-04-29 22:46:26 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+
+import Hero from '@/components/Hero'
+import Newsletter from '@/components/Newsletter'
+import Services from '@/components/Services'
+import Sponsor from '@/components/Sponsor'
+import TravelPoint from '@/components/TravelPoint'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Sponsor />
+      <Services />
+      <TravelPoint />
+      <Newsletter />
+    </>
+  
+  )
+}
